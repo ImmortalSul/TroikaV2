dc9b24b5fe03653d6c553344115ade0f854be49f
ImmortalSul <157292013+ImmortalSul@users.noreply.github.com>
2025-02-11 04:44:01 +0530

    made few changes so the transition is smoother

--- src/constants/perks.ts
+++ src/constants/perks.ts
@@ -1,42 +1,64 @@
 import { Icons } from "@/components";
-import { ZapIcon, ChartSplineIcon, LifeBuoyIcon, PaletteIcon, ShieldCheckIcon, WaypointsIcon, BrainCircuitIcon, SparklesIcon, BrainCircuit, Origami, Github, Computer, Rotate3d, BrainCog, Boxes } from "lucide-react";
+import {
+  ZapIcon,
+  ChartSplineIcon,
+  LifeBuoyIcon,
+  PaletteIcon,
+  ShieldCheckIcon,
+  WaypointsIcon,
+  BrainCircuitIcon,
+  SparklesIcon,
+  BrainCircuit,
+  Origami,
+  Github,
+  Computer,
+  Rotate3d,
+  BrainCog,
+  Boxes,
+} from "lucide-react";
 import React from "react";
 
 export const PERKS = [
-    {
-        icon: Boxes,
-        title: "Optix",
-        description: "Automate workflows visually with drag-and-drop ease and advanced integrations.",
-        link: "/" 
-    },
-    {
-        icon: Origami,
-        title: "Scraipx",
-        description: "Intuitive tool for scraping and organizing web data effortlessly.",
-        link: "/" 
-    },
-    {
-        icon: Computer,
-        title: "RepoSnap",
-        description: "Summarize GitHub repos, docs, and meetings using RAG-powered insights.",
-        link: "/" 
-    },
-    {
-        icon: Rotate3d,
-        title: "Vertex",
-        description: "Stunning 3D portfolios to showcase creativity and skills interactively.",
-        link: "/" 
-    },
-    {
-        icon: BrainCog,
-        title: "Troika AI",
-        description: "Smart assistant trained on Troika products to streamline workflows and provide instant answers.",
-        link: "/" 
-    },
-    {
-        icon: WaypointsIcon,
-        title: "Seamless Integration",
-        description: "Easily integrate with your existing social media platforms and tools.",
-        link: "/" 
-    },
+  {
+    icon: Boxes,
+    title: "Optix",
+    description:
+      "Automate workflows visually with drag-and-drop ease and advanced integrations.",
+    link: "/",
+  },
+  {
+    icon: Origami,
+    title: "Scraipx",
+    description:
+      "Intuitive tool for scraping and organizing web data effortlessly.",
+    link: "https://scrapix.troikahub.tech/",
+  },
+  {
+    icon: Computer,
+    title: "RepoSnap",
+    description:
+      "Summarize GitHub repos, docs, and meetings using RAG-powered insights.",
+    link: "/",
+  },
+  {
+    icon: Rotate3d,
+    title: "Vertex",
+    description:
+      "Stunning 3D portfolios to showcase creativity and skills interactively.",
+    link: "/",
+  },
+  {
+    icon: BrainCog,
+    title: "Troika AI",
+    description:
+      "Smart assistant trained on Troika products to streamline workflows and provide instant answers.",
+    link: "/",
+  },
+  {
+    icon: WaypointsIcon,
+    title: "Seamless Integration",
+    description:
+      "Easily integrate with your existing social media platforms and tools.",
+    link: "/",
+  },
 ];
